9c11b9870869956bf6782b042c2d4f93eb051eb5
Mazin Abdul Azeez <136727661+Maxy747@users.noreply.github.com>
2024-10-23 14:20:37 +0530

    Update Telegram.py

--- MAX/Telegram.py
+++ MAX/Telegram.py
@@ -8,7 +8,7 @@ def app():
     st.write("For academic assistance, you can chat with our Telegram bot:")
     
     # Replace 'your_telegram_bot_link' with the actual link to your Telegram bot
-    telegram_bot_link = "https://t.me/your_telegram_bot_link"
+    telegram_bot_link = "https://t.me/max-spa.bot"
     st.markdown(f"[Click here to chat with the Telegram bot]({telegram_bot_link})", unsafe_allow_html=True)
     
     st.write("The bot can help you with various academic tasks, including reminders, answering questions, and more.")
